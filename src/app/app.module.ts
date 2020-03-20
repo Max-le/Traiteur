7f965cc957f193dcd3f511d4cfc1c74a88e94779
Max <max.09@outlook.xom>
2020-03-20 14:05:12 +0100

    ajout title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { MenuHolderComponent } from './components/menu-holder/menu-holder.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { MenuPizzaComponent } from './components/menu-holder/menu-pizza/menu-pizza.component';
+import { MenuPateComponent } from './components/menu-holder/menu-pate/menu-pate.component';
+import { ContactInfoComponent } from './components/contact/contact-info/contact-info.component';
+import { ContactFormComponent } from './components/contact/contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     MenuHolderComponent,
-    ContactComponent
+    ContactComponent,
+    MenuPizzaComponent,
+    MenuPateComponent,
+    ContactInfoComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/menu-holder/menu-pate/menu-pate.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MenuPateComponent } from './menu-pate.component';
+
+describe('MenuPateComponent', () => {
+  let component: MenuPateComponent;
+  let fixture: ComponentFixture<MenuPateComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MenuPateComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MenuPateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
