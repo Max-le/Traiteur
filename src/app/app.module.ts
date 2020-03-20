2e1a36ef0507f678812c3ca9ebe1498db069287b
Max <max.09@outlook.xom>
2020-03-20 12:00:24 +0100

    group components in a folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { MenuHolderComponent } from './menu-holder/menu-holder.component';
-import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuHolderComponent } from './components/menu-holder/menu-holder.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
